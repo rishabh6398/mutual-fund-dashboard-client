e5da6495ecd3eae4f784d7cb1462964f5a01e366
hrishabh <rishabh6398@gmail.com>
2024-09-21 18:57:52 +0530

    updated route

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<Login />} />
+                <Route path="/mutual-fund-dashboard-client" element={<Login />} />
                 <Route path="/dashboard" element={<Dashboard />} />
             </Routes>
         </Router>
